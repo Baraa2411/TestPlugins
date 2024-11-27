5786d155ae469961e2ba747e94eebd2b197970bc
Baraa2411 <joe5sande.rs57.9.2@gmail.com>
2024-11-27 11:13:37 +0200

    build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,17 +1,12 @@
-dependencies {
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
-}
 // use an integer for version numbers
-version = -1
+version = 1
 
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst")
+    description = "Drama"
+    authors = listOf("Abinis")
 
     /**
     * Status int as the following:
@@ -22,17 +17,12 @@ cloudstream {
     * */
     status = 1
 
-    tvTypes = listOf("Movie")
+    tvTypes = listOf(
+    "Drama",
+    "Asian Drama"
+    )
 
-    requiresResources = true
     language = "en"
 
-    // random cc logo i found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        viewBinding = true
-    }
+    iconUrl = "https://asianc.co/favicon.png"
 }
