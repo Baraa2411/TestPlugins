a5b0debd2acb3096e31b213d017a3bb1792a2678
Baraa2411 <joe5sande.rs57.9.2@gmail.com>
2024-11-27 11:43:24 +0200

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,4 +1,4 @@
-package com.example
+package com.abinis
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
@@ -6,19 +6,25 @@ import com.lagradost.cloudstream3.APIHolder
 import android.content.Context
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.extractorApis
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
-    var activity: AppCompatActivity? = null
-
+class DramacoolPlugin: Plugin() {
     override fun load(context: Context) {
         activity = context as AppCompatActivity
         // All providers should be added in this manner
-        registerMainAPI(ExampleProvider(this))
+        registerMainAPI(DramacoolProvider())
+        addExtractor(ServerHX("https://mxtop3.com"))
+        }
 
         openSettings = { ctx ->
             val frag = BlankFragment(this)
             frag.show(activity!!.supportFragmentManager, "Frag")
         }
+        
+    private fun addExtractor(element: ExtractorApi) {
+        element.sourcePlugin = __filename
+        extractorApis.add(0, element)
     }
 }
