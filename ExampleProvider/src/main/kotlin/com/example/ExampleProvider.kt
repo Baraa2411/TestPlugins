f243faf7caa0a719a21c9ba882257e482dc31eeb
Baraa2411 <joe5sande.rs57.9.2@gmail.com>
2024-11-27 11:24:28 +0200

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,20 +1,25 @@
-package com.example
+package com.abinis
 
 import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
-class ExampleProvider(val plugin: TestPlugin) : MainAPI() { // all providers must be an intstance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
+class DramacoolProvider : MainAPI() { // all providers must be an intstance of MainAPI
+    override var mainUrl = "https://asianc.co/" 
+    override var name = "Dramacool"
+    override val supportedTypes = setOf(
+        TvType.Movie
+        TvType.Drama
+        TvType.Tvshows
+    )
 
-    override var lang = "en"
-
+    
     // enable this when your provider has a main page
     override val hasMainPage = true
-
+    override val hasDownloadSupport = true
+    override val hasQuickSearch = true
+    
     // this function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf<SearchResponse>()
